685a56f24d3261b7f1819d7d9ea7377d25f19400
Ars <arturo@startapps.com.pe>
2023-09-14 13:04:02 -0500

    avance navbar

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,12 +1,23 @@
 const NavBar = () => {
   return (
-    <div className="flex justify-center">
-      <ul className="flex gap-10 ">
-        <li>Home</li>
-        <li>Login</li>
-        <li>Sign Up</li>
-      </ul>
-    </div>
+    <>
+      <div className="bg-indigo-500">
+        <div className="flex mx-5 py-3">
+          <div>
+            logo
+          </div>
+          <ul className="flex">
+            <li>Explorar</li>
+            <li><input type="text" placeholder="Buscar" /></li>
+          </ul>
+          <ul className="flex gap-10 ">
+            <li>Home</li>
+            <li>Login</li>
+            <li>Sign Up</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 export default NavBar;
